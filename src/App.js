49e3172404da1bee6868c3569597d749d630744b
Maria Karoutsou <mkarouts@intrasoft-intl.com>
2021-03-02 21:49:34 +0200

    Adds home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from "./components/Layout";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 const App = () => {
     return (
-        <Layout />
-    )
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+
+    );
 };
 
 export default App;
